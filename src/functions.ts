e62229c694c29446287610c064ce748d80c9382e
Joy <jkmwaria@udel.edu>
2023-02-22 01:05:19 -0500

    completed shout, isQuestion,convertYesNo functions

--- src/functions.ts
+++ src/functions.ts
@@ -17,10 +17,10 @@ export function add3(first: number, second: number, third: number): number {
         sum += first;
     }
     if (second > 0) {
-        sum += first;
+        sum += second;
     }
     if (third > 0) {
-        sum += first;
+        sum += third;
     }
     return sum;
 }
@@ -30,7 +30,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return `${message.toUpperCase()}!`;
 }
 
 /**
@@ -38,7 +38,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.slice(-1) === "?";
 }
 
 /**
@@ -47,5 +47,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    } else if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
